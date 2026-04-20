5ecd8ae518a6e1a7d8d5396866e3873db2226117
Call Center <tutumelchizedek8@gmail.com>
2026-04-20 18:37:57 +0300

    Web socket fix

--- apps/common/routing.py
+++ apps/common/routing.py
@@ -3,5 +3,4 @@ from . import consumers
 
 websocket_urlpatterns = [
     re_path(r'ws/admin/realtime-monitor/$', consumers.RealtimeMonitorConsumer.as_asgi()),
-]</content>
-<parameter name="filePath">c:\Users\callc\Downloads\backend\apps\common\routing.py
+]
